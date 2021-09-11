8ec7e65f4eb5e5101cebabb94edf65c9e349b6b2
Patrick Rose <pjr0911025@googlemail.com>
2021-09-11 12:13:35 +0100

    Fix ResponsiveItemList.tsx
    I forgot to add a key to the sub items

--- src/components/ResponsiveItemList.tsx
+++ src/components/ResponsiveItemList.tsx
@@ -5,7 +5,7 @@ export function ResponsiveItemList({items, itemClasses}: {items: ReactNode[], it
     return <div className="flex flex-col md:flex-row md:flex-wrap w-full justify-center">
         {
             items.map(
-                value => <div className={`w-full md:w-2/4 xl:w-1/3 py-2 px-2 ${itemClasses}`}>
+                (value, key) => <div className={`w-full md:w-2/4 xl:w-1/3 py-2 px-2 ${itemClasses}`} key={key}>
                     {value}
                 </div>
             )
