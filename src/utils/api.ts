e10cdd481779fad2a8ae1b60f3c0b689e0add82b
Patrick Rose <pjr0911025@googlemail.com>
2021-09-11 13:34:40 +0100

    Handle API errors (#14)
    * Handle API errors
    
    Fixes #9
    
    * Don't use the es6 import

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,13 @@
 const rootPath = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3000/api/'
 
-export function apiCall(path: string): Promise<Response> {
-    return fetch(
-        rootPath + path
-    );
+export function apiCall(path: string): {controller: AbortController, response: Promise<Response>} {
+    const controller = new AbortController();
+
+    return {
+        controller,
+        response: fetch(
+            rootPath + path,
+            {signal: controller.signal}
+        )
+    };
 }
