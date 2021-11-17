9b365b3169e4c492172e846673c2962b40fb84b1
Patrick Rose <pjr0911025@googlemail.com>
2021-11-17 12:14:54 +0000

    Use server side rendering for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { isRight } from "fp-ts/Either";
+import { getGamesRepo } from "../server/repository/games";
 import Link from "next/link";
 import Image from "next/image";
 import { LinkDef } from "../utils/utils";
@@ -37,77 +40,33 @@ function HomePageLinks() {
     </div>
 }
 
-type HomeState = {
-    fetched: false | Game[]
-}
-
 type UpcomingEventTypes = Game | number;
 
-abstract class UpcomingEvent<T extends UpcomingEventTypes> extends React.Component<{ value: T }> {
-    render() {
-        return <div className="flex-1 px-3 flex flex-col">
-            <h3 className="text-center text-xl pb-2">{this.getHeading()}</h3>
-            {this.getSubtitle()}
+function Megagame({ value }: { value: Game }) {
+    return <div className="flex-1 px-3 flex flex-col">
+        <h3 className="text-center text-xl pb-2">
+            <Link href={`/games/${value.id}`}><a>{value.name}</a></Link>
+        </h3>
+        <p className="flex-1">{value.preamble}</p>
+        <div className="flex">
+            <Link href={`/games/${value.id}`}>
+                <a className="btn-link m-4 p-4 w-full">Book a space</a>
+            </Link>
         </div>
-    }
-
-    protected abstract getSubtitle(): React.ReactElement;
-
-    protected abstract getHeading(): React.ReactElement;
+    </div>
 }
 
-class Megagame extends UpcomingEvent<Game> {
-    protected getHeading(): React.ReactElement {
-        const { value } = this.props;
-
-        return <Link href={`/games/${value.id}`}><a>{value.name}</a></Link>;
-    }
-
-    protected getSubtitle(): React.ReactElement {
-        const { value } = this.props;
-
-        return <React.Fragment>
-            <p className="flex-1">{value.preamble}</p>
-            <div className="flex">
-                <Link href={`/games/${value.id}`}>
-                    <a className="btn-link m-4 p-4 w-full">Book a space</a>
-                </Link>
-            </div>
-        </React.Fragment>;
-    }
-}
-
-class UnFetched extends UpcomingEvent<number> {
-    protected getHeading(): React.ReactElement {
-        return <div className="w-full"><TextRow className="bg-gray-500 mb-2" /></div>;
-    }
-
-    protected getSubtitle(): React.ReactElement {
-        const repeat = [1, 2, 3]
-        return <React.Fragment>
-            {
-                repeat.map(value => <div key={value} className="w-full"><TextRow className="bg-gray-500 mb-2 h-3"
-                    height={false} /></div>)
-            }
-        </React.Fragment>;
-    }
-}
-
-function UpcomingEvents(props: { events: false | Game[] }) {
-    const toDisplay: UpcomingEventTypes[] = props.events || [1, 2, 3];
-
+function UpcomingEvents({ events }: { events: Game[] }) {
     let eventList;
 
-    if (toDisplay.length === 0) {
+    if (events.length === 0) {
         eventList = <div>
             <p>We don&apos;t have any upcoming events announced yet!</p>
             <p>However, feel free to look at <Link href="/games#past">our past games</Link></p>
         </div>
     } else {
-        eventList = <div className="lg:w-3/4 flex flex-col sm:flex-row"> {toDisplay.map(
-            value => typeof value === 'number'
-                ? <UnFetched value={value} key={value} />
-                : <Megagame value={value} key={value.id} />
+        eventList = <div className="lg:w-3/4 flex flex-col sm:flex-row"> {events.map(
+            value => <Megagame value={value} key={value.id} />
         )
         }
         </div>
@@ -117,77 +76,35 @@ function UpcomingEvents(props: { events: false | Game[] }) {
     return <div className="w-full flex p-4 justify-center">{eventList}</div>
 }
 
-export default class Home extends React.Component<{}, HomeState> {
-    private controller: AbortController | undefined;
+export const getStaticProps: GetStaticProps<{ upcomingGames: Game[] }> = async () => {
+    const upcomingGames = getGamesRepo().upcomingEvents(3);
 
-    constructor(props: {}) {
-        super(props);
-        this.state = { fetched: false };
+    return {
+        props: {
+            upcomingGames: isRight(upcomingGames) ? upcomingGames.right : []
+        },
+        revalidate: 60
     }
+}
 
-    componentDidMount() {
-        if (typeof window !== 'undefined') {
-            this.getUpcomingEvents();
-        }
-    }
-
-    private getUpcomingEvents() {
-        const { controller, response: api } = apiCall('upcoming');
-
-        this.controller = controller;
-
-        api.then(response => {
-            this.controller = undefined;
-            if (!response.ok) {
-                throw new Error('Failed to fetch');
-            }
-
-            return response.json();
-        }).then(value => {
-            if (!UpcomingEventsAPIDecode.is(value)) {
-                throw new Error('API response wasn\'t right? ' + JSON.stringify(value))
-            }
-
-            this.setState({ fetched: value.events });
-        }
-        ).catch(
-            reason => {
-                if (!(reason instanceof DOMException)) {
-                    this.setState({
-                        fetched: []
-                    })
-                }
-            }
-        );
-    }
-
-    componentWillUnmount() {
-        if (this.controller) {
-            this.controller.abort();
-        }
-    }
-
-    render() {
-        const { fetched } = this.state;
-
-        return <React.Fragment>
-            <Hero>
-                <HeroHeading>
-                    OMEGA hosts and produces online megagames.
-                </HeroHeading>
-                <p className="text-xl pb-16">
-                    From grand strategy to roleplay, economics to fireballs, our games blend serious
-                    political simulation with emergent storytelling in settings both mundane and fantastical.
-                </p>
-                <HomePageLinks />
-            </Hero>
-            <MainContent>
-                <h2 className="text-2xl">Our next events</h2>
-                <UpcomingEvents events={fetched} />
-                <div className="w-full flex justify-center">
-                    <Image src={godsWar} className="lg:w-2/4" alt="" />
-                </div>
-            </MainContent>
-        </React.Fragment>;
-    }
+export default function Home({ upcomingGames }: InferGetStaticPropsType<typeof getStaticProps>) {
+    return <React.Fragment>
+        <Hero>
+            <HeroHeading>
+                OMEGA hosts and produces online megagames.
+            </HeroHeading>
+            <p className="text-xl pb-16">
+                From grand strategy to roleplay, economics to fireballs, our games blend serious
+                political simulation with emergent storytelling in settings both mundane and fantastical.
+            </p>
+            <HomePageLinks />
+        </Hero>
+        <MainContent>
+            <h2 className="text-2xl">Our next events</h2>
+            <UpcomingEvents events={upcomingGames} />
+            <div className="w-full flex justify-center">
+                <Image src={godsWar} className="lg:w-2/4" alt="" />
+            </div>
+        </MainContent>
+    </React.Fragment>;
 }
