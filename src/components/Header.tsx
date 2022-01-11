d7a26a91a041cb0b687db60aecf6880eceb7455c
Patrick Rose <pjr0911025@googlemail.com>
2022-01-11 23:04:20 +0000

    Update tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -132,7 +132,7 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex-shrink-0 flex items-center">
+                    <div className="shrink-0 flex items-center">
                         <Image
                             src={omegaLogo} alt="OMEGA logo"
                             height="32"
