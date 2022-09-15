85f5e2ac0ed4d37543e193bed1972cc8925f6140
Shift <shift@laravelshift.com>
2022-09-15 07:19:54 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   safelist: ['__next'],
   theme: {
     extend: {
@@ -13,40 +10,40 @@ module.exports = {
       },
       colors: {
         omega: {
-          light: "#88C9EC",
-          DEFAULT: "#2291CD",
-          dark: "#124E6E"
-        }
+          light: '#88C9EC',
+          DEFAULT: '#2291CD',
+          dark: '#124E6E',
+        },
       },
       maxHeight: {
-        '112': '28rem'
+        112: '28rem',
       },
       minWidth: {
-        '0': '0',
+        0: '0',
         '1/4': '25%',
         '1/2': '50%',
         '3/4': '75%',
-        'full': '100%',
       },
       minHeight: {
-        hero: '12rem'
+        hero: '12rem',
       },
       animation: {
-        placeholder: 'placeholder 2s ease-in-out infinite'
+        placeholder: 'placeholder 2s ease-in-out infinite',
       },
       keyframes: {
         placeholder: {
-          '0%, 100%': { opacity: 0.25},
-          '50%': { opacity: 0.5}
-        }
+          '0%, 100%': {
+            opacity: 0.25,
+          },
+          '50%': {
+            opacity: 0.5,
+          },
+        },
       },
       transitionProperty: {
-        'link': 'opacity, color, background-color, border-color, text-decoration-color'
-      }
+        link: 'opacity, color, background-color, border-color, text-decoration-color',
+      },
     },
   },
-  plugins: [
-      require('@tailwindcss/aspect-ratio'),
-      require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
 }
